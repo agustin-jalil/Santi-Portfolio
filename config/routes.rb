bfba493499370358ec6af5119c708f9e0bae5b56
agustinjalil <agustinjalilleon@gmail.com>
2023-03-27 17:42:46 -0300

    santi-portfolio.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
+  get 'about/index'
+  get 'about/show'
+  get 'work/index'
+  get 'contact/index'
 end
