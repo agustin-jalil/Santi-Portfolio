16aebb17c6e8c4dad5cf665864f77b362e59a583
agustinjalil <agustinjalilleon@gmail.com>
2023-06-10 21:11:19 -0300

    cambios2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get 'about/index'
-  get 'about/show'
   get 'work/index'
+  get 'work/kask'
+  get 'work/roboto'
+  get 'work/blackapa'
+  get 'work/umbrella'
+  get 'work/ppp'
+  get 'work/arboleda'
+  get 'work/snak3'
+  get 'work/mouse'
+  get 'work/heads4'
+  get 'work/panther'
+  get 'work/crunchcarmel'
+  get 'work/owl'
+  get 'about/gallery'
   get 'contact/index'
 end
